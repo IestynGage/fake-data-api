aff35586cc5c15fe92b2712e4d2d4ecb324f8183
Iestyn <>
2023-08-20 21:10:57 +0100

    New Post method to add new endpoint

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const app = express();
 
-
 const generateDataPaths = new Set();
 generateDataPaths.add('/something');
 generateDataPaths.add('/something/poo');
 
 app.get('*', function (req, res) {
   if (generateDataPaths.has(req.path)) {
-    res.send('Found url ' + req.path);
+    res.json({
+      message: 'Found url ' + req.path
+    });
+  } 
+  else {
+    res.status(404).json({
+      message: `Path '${req.path}' not recognised`
+    });
   }
-  res.status(404).send({
-    errorMessage: `Not found url '${req.path}'`
-  });
 });
 
-
 app.post("*", (req, res) => {
-  
+  generateDataPaths.add(req.path);
+  res.json({
+    message: 'url added'
+  })
 })
 
 app.listen(3000);
