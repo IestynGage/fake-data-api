2d623d51a1fdc46f8a00cd2f555b3776ee17dea0
Iestyn <>
2023-08-20 20:46:45 +0100

    Return error if the url is not recongised

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+const generateDataPaths = new Set();
+generateDataPaths.add('/something');
+generateDataPaths.add('/something/poo');
+
+app.get('*', function (req, res) {
+  if (generateDataPaths.has(req.path)) {
+    res.send('Found url ' + req.path);
+  }
+  res.status(404).send({
+    errorMessage: `Not found url '${req.path}'`
+  });
+});
+
+
+app.post("*", (req, res) => {
+  
 })
 
-app.listen(3000)
+app.listen(3000);
