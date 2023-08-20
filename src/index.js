ba8ee3d514dff1cde2164a45373f60dc54269980
Iestyn <>
2023-08-20 21:18:28 +0100

    Add list all paths endpoint

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 const generateDataPaths = new Set();
-generateDataPaths.add('/something');
-generateDataPaths.add('/something/poo');
+
+app.get('/list', (req, res) => {
+  res.json({
+    paths: [...generateDataPaths]
+  })
+});
 
 app.get('*', function (req, res) {
   if (generateDataPaths.has(req.path)) {
@@ -19,10 +23,18 @@ app.get('*', function (req, res) {
 });
 
 app.post("*", (req, res) => {
-  generateDataPaths.add(req.path);
-  res.json({
-    message: 'url added'
-  })
+  const path = req.path;
+  if (path === "/list") {
+    res.status(400).json({
+      message: `URL '${path}' added`
+    });
+  } 
+  else {
+    generateDataPaths.add(req.path);
+    res.json({
+      message: `URL '${path}' added`
+    });
+  }
 })
 
 app.listen(3000);
